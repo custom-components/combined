f5f3447bd5876e292df140f8b5e3a47fbdba370f
ludeeus <joasoe@gmail.com>
2018-08-07 09:43:56 +0200

    typo

--- custom_components/camera/combined.py
+++ custom_components/camera/combined.py
@@ -27,7 +27,7 @@ PLATFORM_SCHEMA = PLATFORM_SCHEMA.extend({
 })
 
 def setup_platform(hass, config, add_devices, discovery_info=None):
-    """Set up the Camera that works with local files."""
+    """Set up the Camera platform."""
     entities = config.get(CONF_ENTITIES)
     name = config.get(CONF_NAME)
     base_address = config.get(CONF_BASE_ADDRESS)
